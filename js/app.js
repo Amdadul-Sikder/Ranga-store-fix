687bf4640a639b2df2e74c6796af040ce2ad239c
Amdadul Sikder <amdadulsikder3@gmail.com>
2021-09-13 20:57:25 +0600

    search btn redesign

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const loadProducts = () => {
   showProducts(data);
 };
 
-
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
@@ -35,20 +34,18 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
-
-
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
 // main price update function
+
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
@@ -57,15 +54,15 @@ const updatePrice = (id, value) => {
   document.getElementById(id).innerText = total.toFixed(2);
 };
 
-
 // set innerText function
+
 const setInnerText = (id, value) => {
   // document.getElementById(id).innerText = Math.round(value);
   document.getElementById(id).innerText = value.toFixed(2);
 };
 
+// // update delivery charge and total Tax
 
-// update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
   if (priceConverted > 200) {
@@ -83,6 +80,7 @@ const updateTaxAndCharge = () => {
 };
 
 //grandTotal update function
+
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
